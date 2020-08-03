1ce4a5a8793b75e3b845de0b17fa2959cc3e9719
harshy250 <yadav.harshgst@gmail.com>
2020-08-03 19:41:22 +0530

    Minimax done successfully

--- src/com/chess/engine/player/ai/MiniMax.java
+++ src/com/chess/engine/player/ai/MiniMax.java
@@ -7,9 +7,11 @@ import com.chess.engine.player.MoveTransition;
 public class MiniMax implements MoveStrategy {
 
     private final BoardEvaluator boardEvaluator;
+    private final int searchDepth;
 
-    public MiniMax() {
+    public MiniMax(final int searchDepth) {
         this.boardEvaluator = new StandardBoardEvaluator();
+        this.searchDepth = searchDepth;
     }
 
     @Override
@@ -18,8 +20,9 @@ public class MiniMax implements MoveStrategy {
     }
 
     @Override
-    public Move execute(Board board, int depth) {
+    public Move execute(Board board) {
         final long startTime = System.currentTimeMillis();
+
 
         Move bestMove = null;
 
@@ -27,7 +30,7 @@ public class MiniMax implements MoveStrategy {
         int lowestSeenValue = Integer.MAX_VALUE;
         int currentValue;
 
-        System.out.println(board.currentPlayer() + "Thinking with depth" +depth);
+        System.out.println(board.currentPlayer() + "Thinking with depth" + this.searchDepth);
 
         int numMoves = board.currentPlayer().getLegalMoves().size();
 
@@ -37,8 +40,8 @@ public class MiniMax implements MoveStrategy {
             if(moveTransition.getMoveStatus().isDone()) {
 
                 currentValue= board.currentPlayer().getAlliance().isWhite() ?
-                                min(moveTransition.getTransitionBoard(), depth-1) :
-                                max(moveTransition.getTransitionBoard(), depth-1);
+                                min(moveTransition.getTransitionBoard(), this.searchDepth-1) :
+                                max(moveTransition.getTransitionBoard(), this.searchDepth-1);
 
                 if(board.currentPlayer().getAlliance().isWhite() && currentValue >= highestSeenValue) {
                     highestSeenValue = currentValue;
@@ -48,10 +51,7 @@ public class MiniMax implements MoveStrategy {
                     lowestSeenValue = currentValue;
                     bestMove = move;
                 }
-
             }
-
-
         }
         final long executionTime = System.currentTimeMillis() - startTime;
 
@@ -60,7 +60,7 @@ public class MiniMax implements MoveStrategy {
 
     public int min(final Board board,
                    final int depth){
-        if(depth == 0 /* || Game Over */) {
+        if(depth == 0 || isEndGameScenario(board)) {
             this.boardEvaluator.evaluate(board, depth);
         }
         int lowestSeenValue = Integer.MAX_VALUE;
@@ -77,9 +77,14 @@ public class MiniMax implements MoveStrategy {
         return lowestSeenValue;
     }
 
+    public static boolean isEndGameScenario(final Board board) {
+        return board.currentPlayer().isInCheckMate() ||
+                board.currentPlayer().isInStaleMate();
+    }
+
     public int max(final Board board,
                    final int depth){
-        if(depth == 0 /* || Game Over */) {
+        if(depth == 0 || isEndGameScenario(board)) {
             this.boardEvaluator.evaluate(board, depth);
         }
         int highestSeenValue = Integer.MIN_VALUE;
--- src/com/chess/engine/player/ai/StandardBoardEvaluator.java
+++ src/com/chess/engine/player/ai/StandardBoardEvaluator.java
@@ -5,6 +5,12 @@ import com.chess.engine.pieces.Piece;
 import com.chess.engine.player.Player;
 
 public class StandardBoardEvaluator implements BoardEvaluator {
+
+    private static final int CHECK_BONUS = 50;
+    private static final int CHECKMATE_BONUS = 10000;
+    private static final int DEPTH_BONUS = 100;
+    private static final int CASTLE_BONUS = 60;
+
     @Override
     public int evaluate(final Board board,
                         final int depth) {
@@ -15,8 +21,33 @@ public class StandardBoardEvaluator implements BoardEvaluator {
     private int scorePlayer(final Board board,
                             final Player player,
                             final int depth) {
-        return pieceValue(player);
+        return pieceValue(player) +
+                mobility(player) +
+                check(player) +
+                checkmate(player, depth) +
+                castled(player);
         // + checkmate, check, castles, mobility...
+    }
+
+    private static int castled(Player player) {
+        return player.isCastled() ? CASTLE_BONUS : 0;
+
+    }
+
+    private static int checkmate(Player player, int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECKMATE_BONUS * depthBonus(depth) : 0;
+    }
+
+    private static int depthBonus(int depth) {
+        return depth == 0 ?  1 : DEPTH_BONUS * depth;
+    }
+
+    private static int check(Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+
+    private static int mobility(Player player) { // How many options a player has
+        return player.getLegalMoves().size();
     }
 
     private static int pieceValue(final Player player){
@@ -24,5 +55,6 @@ public class StandardBoardEvaluator implements BoardEvaluator {
         for(final Piece piece : player.getActivePieces()) {
             pieceValueScore += piece.getPieceValue();
         }
+        return  pieceValueScore;
     }
 }
